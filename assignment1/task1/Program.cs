58327ab1c5cd6498f9df0948ba7f66064c14228f
Rowchange <3137115525@qq.com>
2025-02-25 22:35:05 +0800

    提交Assignment1作业

---
+++ assignment1/task1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("请输入第一个数字：");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("请输入运算符（+、-、*、/）：");
+        string op = Console.ReadLine();
+
+        Console.Write("请输入第二个数字：");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+        switch (op)
+        {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                if (num2 == 0)
+                    Console.WriteLine("错误：除数不能为零！");
+                else
+                    result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("无效运算符！");
+                return;
+        }
+
+        Console.WriteLine($"计算结果：{result}");
+    }
+}
---
+++ assignment1/task2/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!double.TryParse(textBox1.Text, out double num1) ||
+       !double.TryParse(textBox2.Text, out double num2))
+            {
+                label1.Text = "输入无效！";
+                return;
+            }
+
+            string op = comboBox1.SelectedItem?.ToString();
+            if (op == null)
+            {
+                label1.Text = "请选择运算符！";
+                return;
+            }
+
+            try
+            {
+                double result = 0;
+                switch (op)
+                {
+                    case "+": result = num1 + num2; break;
+                    case "-": result = num1 - num2; break;
+                    case "*": result = num1 * num2; break;
+                    case "/": result = num1 / num2; break;
+                }
+                label1.Text = $"结果：{result}";
+            }
+            catch (DivideByZeroException)
+            {
+                label1.Text = "除数不能为零！";
+            }
+        }
+    }
+}
